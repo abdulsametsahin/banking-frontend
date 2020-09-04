894c5fad55d3a662444fb6cf9e6ac330bddc1510
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-04 03:00:22 +0300

    create index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,50 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        frontend
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="container d-flex justify-content-center align-items-center h-100">
+    <div class="d-flex justify-content-center w-100">
+      <div class="col-md-6 col-xs-12">
+        <div class="card w-100">
+          <div class="card-header">Login</div>
+          <div class="card-body">
+            <div class="form-group">
+              <label for="account_id">Account ID:</label>
+              <input type="number" class="form-control" v-model="account_id" />
+            </div>
+
+            <button class="btn btn-info" @click="login">🙋🏼‍♂️Login</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from "~/plugins/axios";
+
+export default {
+  data() {
+    return {
+      is_loading: false,
+      account_id: null,
+    };
+  },
+  methods: {
+    login(event) {
+      axios
+        .get("accounts/" + this.account_id)
+        .then((res) => {
+          this.$store.commit("auth/login", res.data.data.account);
+          this.$router.push({ path: "/account" });
+        })
+        .catch((err) => {
+          this.$bvToast.toast("User not found!", {
+            title: "Warning",
+            variant: "warning",
+            autoHideDelay: 3000,
+            appendToast: false,
+          });
+        });
+    },
+  },
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
