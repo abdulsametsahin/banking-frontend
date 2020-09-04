cb409d96cca18e30f8ad42cd41a2ecc292b45bf4
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-04 03:00:29 +0300

    create auth state

---
+++ store/auth.js
@@ -0,0 +1,14 @@
+export const state = () => {
+  account: null
+}
+
+export const mutations = {
+  login(state, account) {
+    state.account = account;
+    localStorage.setItem('account', JSON.stringify(account));
+  },
+  logout(state) {
+    state.account = null;
+    localStorage.removeItem('account');
+  }
+}
