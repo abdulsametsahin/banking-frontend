a5e4341111b88766a8c93e3336c793f4ec17a66e
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-09-05 15:32:20 +0300

    create Header component

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container">
+      <div class="d-flex w-100 justify-content-between alin-center">
+        <h4>{{ account.name }} ({{ account.balance }})</h4>
+        <div>
+          <button class="btn btn-danger" @click="logout">Logout</button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: "Header",
+  computed: {
+    account() {
+      return this.$store.state.auth.account;
+    }
+  },
+  methods: {
+    logout(event) {
+      event.preventDefault();
+      this.$store.commit("auth/logout");
+      this.$router.push({path: "/"});
+    },
+  }
+}
+</script>
